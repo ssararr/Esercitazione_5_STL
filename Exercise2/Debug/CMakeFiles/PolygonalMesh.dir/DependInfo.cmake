ff68fba2741a1dc457f802c684c989c113642725
ssararr <s311121@studenti.polito.it>
2025-04-17 23:06:04 +0200

    tentstivo

---
+++ Exercise2/Debug/CMakeFiles/PolygonalMesh.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/appuser/Data/esercitazioni/esercitazione5/Exercise2/ExportParaview/UCDUtilities.cpp" "CMakeFiles/PolygonalMesh.dir/ExportParaview/UCDUtilities.cpp.o" "gcc" "CMakeFiles/PolygonalMesh.dir/ExportParaview/UCDUtilities.cpp.o.d"
+  "/home/appuser/Data/esercitazioni/esercitazione5/Exercise2/main.cpp" "CMakeFiles/PolygonalMesh.dir/main.cpp.o" "gcc" "CMakeFiles/PolygonalMesh.dir/main.cpp.o.d"
+  "/home/appuser/Data/esercitazioni/esercitazione5/Exercise2/src/Utils.cpp" "CMakeFiles/PolygonalMesh.dir/src/Utils.cpp.o" "gcc" "CMakeFiles/PolygonalMesh.dir/src/Utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
